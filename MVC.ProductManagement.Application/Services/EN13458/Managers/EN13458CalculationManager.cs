e88f318c1af239358ccb2399bbd784e3e21c1ca4
remziurhan1 <urremzi@gmail.com>
2026-03-12 16:26:57 +0300

    EN13458: derive E and K from selected outer shell material

--- MVC.ProductManagement.Application/Services/EN13458/Managers/EN13458CalculationManager.cs
+++ MVC.ProductManagement.Application/Services/EN13458/Managers/EN13458CalculationManager.cs
@@ -30,6 +30,9 @@ namespace MVC.ProductManagement.Application.Services.EN13458.Managers
             input.OuterShellMaterialStrength = await _strengthProvider.ResolveEffectiveYieldStrengthAsync(input.OuterShellMaterialId, input.OuterShellMaterialFormId, input.IsColdStretchApplied);
             input.OuterHeadMaterialStrength = await _strengthProvider.ResolveEffectiveYieldStrengthAsync(input.OuterHeadMaterialId, input.OuterHeadMaterialFormId, input.IsColdStretchApplied);
 
+            input.YieldFactorK = input.OuterShellMaterialStrength ?? input.YieldFactorK;
+            input.ElasticModulus = await _strengthProvider.ResolveElasticModulusAsync(input.OuterShellMaterialId);
+
             var result = await _engine.CalculateAsync(input);
             ApplySectorOrientationOutputs(result, input);
             return result;
