c878d7b2386dc15ff45d439bae9e773963e01dec
remziurhan1 <urremzi@gmail.com>
2026-03-17 16:10:58 +0300

    Kod üretme akışını StockSubCodeRule için ayrı view ekranına taşı

--- MVC.ProductManagement.Application/Services/StockCodes/Catalog/IStockSubCodeRuleService.cs
+++ MVC.ProductManagement.Application/Services/StockCodes/Catalog/IStockSubCodeRuleService.cs
@@ -6,6 +6,7 @@ namespace MVC.ProductManagement.Application.Services.StockCodes.Catalog
     {
         Task<List<StockSubCodeRuleListDto>> GetAllAsync(Guid? subGroupId = null);
         Task<StockSubCodeRuleDetailDto?> GetByIdAsync(Guid id);
+        Task<StockSubCodeRuleDetailDto?> FindBySubGroupAndDescriptionAsync(Guid subGroupId, string? description);
         Task<StockSubCodeRuleDetailDto> CreateAsync(StockSubCodeRuleCreateDto dto);
         Task<StockSubCodeRuleDetailDto> UpdateAsync(StockSubCodeRuleUpdateDto dto);
         Task<string> GetNextStockCodeBySubGroupAsync(Guid subGroupId);
